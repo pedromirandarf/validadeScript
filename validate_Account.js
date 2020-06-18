6efcb9c0fefc46cb25b35f87ecc132fae9bd23e8
pedromirandarf <pedro.celsomrf@gmail.com>
2020-06-18 09:24:23 -0300

    Script final

--- validate_Account.js
+++ validate_Account.js
@@ -11,23 +11,27 @@ document.addEventListener('change', async() => {
         ar = dominioC.split(".");
         dominioC = (ar[0] === 'www')  ? ar[1] : ar[0];
         dominioC = (ar[1] === 'com')  ? ar[0] : ar[1];
-        console.log(dominioC);
-        console.log(dominioC);
     }
     if(dominioC == "gmail" || dominioC == "hotmail" || dominioC == "outlook" || dominioC == "live" || dominioC == "yahoo"){
         document.getElementsByName('email')[0]['value'] = "";
         window.alert("Utilize um email institucional");
     }else{
         await axios.get(`https://compugrafapi.herokuapp.com/account/bd/?id=${dominioC}`).then(response => {
-        //console.log(response.data.Data[0].Empresa);
-        if(document.getElementsByName('customer_account').length !== 0){
-            document.getElementsByName('customer_account')[0]['value'] = response.data.Data[0].Empresa;;    
-        }else if(document.getElementsByName('field[50]').length !== 0){
-            document.getElementsByName('field[50]')[0]['value'] =response.data.Data[0].Empresa;;
+             console.log(response.data.Data[0].Empresa);
+             console.log(document.getElementsByName('customer_account').length);
+             console.log(document.getElementsByName('field[50]').length);
+             console.log(document.getElementsByName('company').length);
+            if(document.getElementsByName('customer_account').length != 0){
+                console.log("customer_account");
+                document.getElementsByName('customer_account')[0]['value'] = response.data.Data[0].Empresa;;    
+            }else if(document.getElementsByName('field[50]').length != 0){
+                console.log("field[50]");
+                document.getElementsByName('field[50]')[0]['value'] =response.data.Data[0].Empresa;;
 
-        }else if(document.getElementsByName('company')[0]['value'].length !== 0){
-            document.getElementsByName('company')[0]['value'] =response.data.Data[0].Empresa;;
-        }va
+            }else if(document.getElementsByName('company').length != 0){
+                console.log("company");
+                document.getElementsByName('company')[0]['value'] =response.data.Data[0].Empresa;;
+            }
         })
         .catch(error => {
             dominioC = dominioC.substring(0,1).toUpperCase().concat(dominioC.substring(1));
